2f4a4ded54e7ce6079daecd29b1eda031a76fbb9
Carlos Calderon <cdcalderon@gmail.com>
2019-03-16 23:20:16 -0500

    Add BankLoan model

---
+++ src/app/shared/models/bank-loan-model.ts
@@ -0,0 +1,27 @@
+export interface BankLoan {
+  loanType: string;
+  payment: number;
+  cms: number;
+  perKw: number;
+  dealerFeePercent: number;
+  interest: number;
+  term: number;
+  kWhSize: number;
+  ppw: number;
+  base: number;
+  priceBeforeRoof: number;
+  netSystemPrice: number;
+  FederalTaxCredit: number;
+  nvRebate: number;
+  totalAfterCredits: number;
+  dealerFee: number;
+  dealerCost: number;
+  audit: number;
+  roofCost: number;
+  leadSetter: number;
+  wishedPpw: number;
+}
+
+
+
+
