5cb64b802bdbf1e3eeb6c8a12f1ac18a8ca89fd8
carlos calderon <cdcalderon@gmail.com>
2019-03-23 04:47:08 -0500

    implement Wishedppw and initial kwhsize updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { BankLoan } from './shared/models/bank-loan-model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
   title = 'next-solar-calculator';
@@ -12,6 +12,8 @@ export class AppComponent implements OnInit {
   isLeadSetter: boolean = false;
   isFederalCredit: boolean = true;
   isNVRebate: boolean = false;
+  isOverrideBasePriceActive: boolean = false;
+  isOverridekWhSizeActive: boolean = false;
   setter: number = 350;
   desiredKcms: number = 700;
   basePrice: number = 2.5;
@@ -47,7 +49,7 @@ export class AppComponent implements OnInit {
       dealerFeePercent: 16.50 / 100,
       interest: 3.99 / 100,
       term: 240,
-      kWhSize: 9.365,
+      kWhSize: 6.365,
       ppw: 4.80,
       base: 2.50,
       priceBeforeRoof: 0,
@@ -65,39 +67,40 @@ export class AppComponent implements OnInit {
   ];
 
   ngOnInit(): void {
+    this.calculateLoans();
     // let completeLoans: BankLoan[] = [];
-    this.bankLoans = this.bankLoans.map(bl => {
-      const netSystemPrice = this.calculateNetSystemPrice(bl.kWhSize, bl.ppw, bl.dealerFeePercent, bl.roofCost);
-      const nvRebate = this.calculateNvRebate();
-      const federalTaxCredit = this.calculateFederalTaxCredit(netSystemPrice);
-      const priceBeforeRoof = this.calculatePriceBeforeRoof(bl.kWhSize, bl.ppw);
-      const dealerFee = this.calculateDealerFeeAmount(priceBeforeRoof, bl.dealerFeePercent);
-      const totalAfterCredits = this.calculateTotalAfterCredit(nvRebate, federalTaxCredit, netSystemPrice);
-      const cms = this.calculateCms(bl.kWhSize, bl.ppw, bl.base, dealerFee, bl.audit, bl.leadSetter);
-      return <BankLoan>{
-        loanType: bl.loanType,
-        dealerFeePercent: bl.dealerFeePercent,
-        interest: bl.interest,
-        term: bl.term,
-        kWhSize: bl.kWhSize,
-        ppw: bl.ppw,
-        base: bl.base,
-        dealerCost: bl.dealerCost,
-        audit: bl.audit,
-        roofCost: bl.roofCost,
-        leadSetter: this.calculateLeadSetter(),
-        priceBeforeRoof: this.calculatePriceBeforeRoof(bl.kWhSize, bl.ppw),
-        netSystemPrice,
-        federalTaxCredit,
-        nvRebate,
-        totalAfterCredits,
-        dealerFee,
-        wishedPpw: this.calculateWishedPpw(bl.audit, dealerFee),
-        payment: this.calculatePayment(bl.interest, 12, bl.term, totalAfterCredits),
-        cms: cms,
-        perKw: this.calculatePerKW(cms)
-      };
-    });
+    // this.bankLoans = this.bankLoans.map(bl => {
+    //   const netSystemPrice = this.calculateNetSystemPrice(bl.kWhSize, bl.ppw, bl.dealerFeePercent, bl.roofCost);
+    //   const nvRebate = this.calculateNvRebate();
+    //   const federalTaxCredit = this.calculateFederalTaxCredit(netSystemPrice);
+    //   const priceBeforeRoof = this.calculatePriceBeforeRoof(bl.kWhSize, bl.ppw);
+    //   const dealerFee = this.calculateDealerFeeAmount(priceBeforeRoof, bl.dealerFeePercent);
+    //   const totalAfterCredits = this.calculateTotalAfterCredit(nvRebate, federalTaxCredit, netSystemPrice);
+    //   const cms = this.calculateCms(bl.kWhSize, bl.ppw, bl.base, dealerFee, bl.audit, bl.leadSetter);
+    //   return <BankLoan>{
+    //     loanType: bl.loanType,
+    //     dealerFeePercent: bl.dealerFeePercent,
+    //     interest: bl.interest,
+    //     term: bl.term,
+    //     kWhSize: bl.kWhSize,
+    //     ppw: bl.ppw,
+    //     base: bl.base,
+    //     dealerCost: bl.dealerCost,
+    //     audit: bl.audit,
+    //     roofCost: bl.roofCost,
+    //     leadSetter: this.calculateLeadSetter(),
+    //     priceBeforeRoof: this.calculatePriceBeforeRoof(bl.kWhSize, bl.ppw),
+    //     netSystemPrice,
+    //     federalTaxCredit,
+    //     nvRebate,
+    //     totalAfterCredits,
+    //     dealerFee,
+    //     wishedPpw: this.calculateWishedPpw(bl.audit, bl.dealerFeePercent),
+    //     payment: this.calculatePayment2(bl.interest / 12, bl.term, totalAfterCredits, 0, 0),
+    //     cms: cms,
+    //     perKw: this.calculatePerKW(cms)
+    //   };
+    // });
   }
 
   // =-(PMT(3.79%/12,240,21386))*1.035
@@ -117,10 +120,24 @@ export class AppComponent implements OnInit {
     return pmt;
   }
 
-  calculatePerKW(cms: number): number {
-    return cms / this.kWhSize;
+  calculatePayment2(ratePerPeriod: number, numberOfPayments: number, presentValue: number, futureValue: number, type: number) {
+    if (ratePerPeriod != 0.0) {
+      // Interest rate exists
+      const q = Math.pow(1 + ratePerPeriod, numberOfPayments);
+      return -(ratePerPeriod * (futureValue + (q * presentValue))) / ((-1 + q) * (1 + ratePerPeriod * (type)));
+
+    } else if (numberOfPayments != 0.0) {
+      // No interest rate, but number of payments exists
+      return -(futureValue + presentValue) / numberOfPayments;
+    }
+
+    return 0;
   }
 
+  calculatePerKW(cms: number, kWhSize: number): number {
+    return cms / kWhSize;
+  }
+  // =((6.365*1000)*(4.8-8))-5958-0
   calculateCms(
     kWhSize: number,
     ppw: number,
@@ -141,8 +158,8 @@ export class AppComponent implements OnInit {
     return priceBeforeRoof * dealerFeePercent;
   }
 
-  calculateLeadSetter() {
-    return this.kWhSize * this.setter;
+  calculateLeadSetter(kWhSize: number) {
+    return kWhSize * this.setter;
   }
 
   calculatePriceBeforeRoof(kWhSize: number, ppw: number) {
@@ -162,8 +179,8 @@ export class AppComponent implements OnInit {
     return netSystemPrice * (30 / 100);
   }
 
-  calculateNvRebate() {
-    return this.kWhSize * 200;
+  calculateNvRebate(kWhSize: number) {
+    return kWhSize * 200;
   }
 
   calculateTotalAfterCredit(
@@ -182,7 +199,84 @@ export class AppComponent implements OnInit {
     }
   }
 
-  calculateWishedPpw(audit: number, dealerFee: number) {
-    return ((this.desiredKcms / 1000) + this.basePrice + (audit / (this.kWhSize * 1000))) / (1 - dealerFee);
+  calculateWishedPpw(audit: number, dealerFeePercent: number, kWhSize: number) {
+    return ((this.desiredKcms / 1000) + this.basePrice + (audit / (kWhSize * 1000))) / (1 - dealerFeePercent);
+  }
+
+  onIsLeadSetterChange(event: any) {
+    console.log('onIsLeadSetterChange ', event);
+    this.calculateLoans();
+  }
+
+  onIsFederalCreditChange(event: any) {
+    console.log('onIsFederalCreditChange ', event);
+    this.calculateLoans();
+  }
+
+  onIsNvRebateChange(event: any) {
+    console.log('onIsNvRebateChange ', event);
+    this.calculateLoans();
+  }
+
+  onBasePriceChange(event: any) {
+    console.log('onBasePriceChange ', event);
+    if (this.isOverrideBasePriceActive) {
+      this.calculateLoans();
+    }
+  }
+
+  onisOverrideBasePriceActiveChange(event: any) {
+    console.log('onisOverrideBasePriceActiveChange ', event);
+    this.calculateLoans();
+  }
+
+  onisOverridekWhSizeActiveChange(event: any) {
+    console.log('onisOverridekWhSizeActiveChange ', event);
+    this.calculateLoans();
+  }
+
+  onkWhSizeChange(event: any) {
+    console.log('onkWhSizeChangeChange ', event);
+    if (this.isOverridekWhSizeActive) {
+      this.calculateLoans();
+    }
+  }
+
+  calculateLoans() {
+    this.bankLoans = this.bankLoans.map(bl => {
+      bl.kWhSize = this.isOverridekWhSizeActive ? this.kWhSize : bl.kWhSize;
+      bl.base = this.isOverrideBasePriceActive ? this.basePrice : bl.base;
+      const netSystemPrice = this.calculateNetSystemPrice(bl.kWhSize, bl.ppw, bl.dealerFeePercent, bl.roofCost);
+      const nvRebate = this.calculateNvRebate(bl.kWhSize);
+      const federalTaxCredit = this.calculateFederalTaxCredit(netSystemPrice);
+      const priceBeforeRoof = this.calculatePriceBeforeRoof(bl.kWhSize, bl.ppw);
+      const dealerFee = this.calculateDealerFeeAmount(priceBeforeRoof, bl.dealerFeePercent);
+      const totalAfterCredits = this.calculateTotalAfterCredit(nvRebate, federalTaxCredit, netSystemPrice);
+      const cms = this.calculateCms(bl.kWhSize, bl.ppw, bl.base, dealerFee, bl.audit, bl.leadSetter);
+
+      return <BankLoan>{
+        loanType: bl.loanType,
+        dealerFeePercent: bl.dealerFeePercent,
+        interest: bl.interest,
+        term: bl.term,
+        kWhSize: bl.kWhSize,
+        ppw: bl.ppw,
+        base: bl.base,
+        dealerCost: bl.dealerCost,
+        audit: bl.audit,
+        roofCost: bl.roofCost,
+        leadSetter: this.calculateLeadSetter(bl.kWhSize),
+        priceBeforeRoof: this.calculatePriceBeforeRoof(bl.kWhSize, bl.ppw),
+        netSystemPrice,
+        federalTaxCredit,
+        nvRebate,
+        totalAfterCredits,
+        dealerFee,
+        wishedPpw: this.calculateWishedPpw(bl.audit, bl.dealerFeePercent, bl.kWhSize),
+        payment: this.calculatePayment2(bl.interest / 12, bl.term, totalAfterCredits, 0, 0),
+        cms: cms,
+        perKw: this.calculatePerKW(cms, bl.kWhSize)
+      };
+    });
   }
 }
